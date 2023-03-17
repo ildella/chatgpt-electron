0dbe13c33f6fb3e5b058867593c382b92de0aa15
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-03-17 15:45:58 +0100

    store window size

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
+const Store = require('electron-store')
+const store = new Store()
 
 app.on('ready', () => {
+  const {width = 1200, height = 1200} = store.get('windowSize') || {}
   const mainWindow = new BrowserWindow({ 
-    width: 1200, 
-    height: 1200,
+    width, 
+    height,
     icon: './favicon-32x32.png',
-  });
-  mainWindow.loadURL('https://chat.openai.com');
-});
+  })
+  mainWindow.loadURL('https://chat.openai.com')
+  mainWindow.on('close', () => {
+    const { width, height } = mainWindow.getBounds()
+    store.set('windowSize', { width, height })
+  })
+})
